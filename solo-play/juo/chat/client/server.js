72cb1f6ccf1a943bccd3f688c3e4e579b03041b4
juo <juo6442@gmail.com>
2021-09-19 13:44:30 +0900

    [juo]
    Add node server - I just Ctrl CVed

---
+++ solo-play/juo/chat/client/server.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+const http = require("http");
+
+class Server {
+    constructor(config) {
+        this.index = config.index ?? "/index.html";
+        this.port = config.port ?? 80;
+
+        this.handlers = new Map();
+        this.server = http.createServer(this.onRequestIncoming.bind(this));
+    }
+
+    start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server is running on ${this.port} port`);
+        });
+    }
+
+    stop() {
+        this.server.close(() => {
+            console.log(`Server is closed`);
+        });
+    }
+
+    setHandler(url, handler) {
+        this.handlers.set(url, handler);
+    }
+
+    getHandler(url) {
+        const handler = this.handlers.get(url) ?? this.defaultHandler;
+        return handler.bind(this);
+    }
+
+    onRequestIncoming(request, response) {
+        const buffer = [];
+
+        request.on("error", err => {
+            console.error(err);
+        }).on("data", chunk => {
+            buffer.push(chunk);
+        }).on("end", () => {
+            const requestParam = {
+                url: request.url,
+                method: request.method,
+                headers: request.headers,
+                body: Buffer.concat(buffer).toString(),
+            }
+
+            this.getHandler(request.url)(requestParam, response);
+        });
+        response.on("error", err => {
+            console.error(err);
+        });
+    }
+
+    defaultHandler(request, response) {
+        let url = request.url;
+        if (url === "/") url = this.index;
+        url = "." + url;
+
+        fs.readFile(url, (err, data) => {
+            if (err) {
+                response.statusCode = 404;
+                response.end("Invalid addreess");
+            } else {
+                response.end(data);
+            }
+        });
+    }
+}
+
+module.exports = Server;
