1d250d3600d9fde71e900fe1dbd138e53de07f35
juo <juo6442@gmail.com>
2021-10-14 00:50:49 +0900

    Implement session key management

--- solo-play/juo/member/server.js
+++ solo-play/juo/member/server.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const http = require("http");
+const Cookie = require("./cookie.js");
+const Session = require("./session.js");
+const SessionManager = require("./sessionmanager.js");
 
 class Server {
     constructor(config) {
@@ -8,6 +11,7 @@ class Server {
         this.port = config.port ?? 80;
 
         this.handlers = new Map();
+        this.sessionManager = new SessionManager();
         this.server = http.createServer(this.onRequestIncoming.bind(this));
     }
 
@@ -60,6 +64,15 @@ class Server {
     }
 
     defaultHandler(request, response) {
+        const cookie = Cookie.from(request.headers.cookie);
+        const session = this.sessionManager.getSession(cookie.get(Session.COOKIE_ID));
+        if (session) {
+            console.log(`Request comes in session ${session.id}`);
+        } else {
+            const newSession = this.sessionManager.createSession();
+            response.setHeader("Set-Cookie", newSession.toCookie());
+        }
+
         fs.readFile(request.url, (err, data) => {
             if (err) {
                 this.respondWithError(response, 404);
@@ -70,7 +83,6 @@ class Server {
     }
 
     respondWithError(response, code) {
-        const url = this.refineUrl(`/${code}.html`);
         response.statusCode = code;
         response.end(`Error: ${code}`);
     }
