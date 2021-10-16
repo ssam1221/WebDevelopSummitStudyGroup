c0b50f9f2758f5201a5768e6b42942550c57a756
juo <juo6442@gmail.com>
2021-10-17 00:52:22 +0900

    Make web server module

--- solo-play/juo/homework_10/main.js
+++ solo-play/juo/homework_10/main.js
@@ -1,19 +1,6 @@
-const Crypto = require("crypto");
-const Express = require("express");
-const Session = require("express-session");
+const WebServer =require("./web_server.js");
 
-const app = Express();
-const port = 8080;
-
-app.use(Session({
-    secret: "some_secret",
-    resave: false,
-    saveUninitialized: true
-}));
-app.use(require("./router/session.js"));
-
-app.use(Express.static(__dirname + "/web"));
-
-app.listen(port, () => {
-    console.log(`Server is running on ${port} port`);
-});
+new WebServer({
+    root: "web",
+    port: 8080,
+}).start();
