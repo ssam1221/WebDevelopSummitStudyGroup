1a20f51431ad8d48e0b2ebbbc35ca0ecbf8d755a
juo <juo6442@gmail.com>
2021-10-17 00:32:18 +0900

    Implement session

--- solo-play/juo/homework_10/main.js
+++ solo-play/juo/homework_10/main.js
@@ -1,9 +1,31 @@
-const express = require("express");
+const Crypto = require("crypto");
+const Express = require("express");
+const Session = require("express-session");
 
-const app = express();
+const app = Express();
 const port = 8080;
 
-app.use(express.static(__dirname + "/web"));
+app.use(Session({
+    secret: "some_secret",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(function(req, res, next) {
+    session = req.session;
+
+    if (session.testValue) {
+        console.log("Existing session: " + session.testValue);
+    } else {
+        const id = Crypto.randomBytes(16).toString("base64");
+        session.testValue = id;
+        console.log("It's a new session so give a new test value: " + id);
+    }
+
+    next();
+});
+
+app.use(Express.static(__dirname + "/web"));
 
 app.listen(port, () => {
     console.log(`Server is running on ${port} port`);
