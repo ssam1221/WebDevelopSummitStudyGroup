0095198e59721b876932290e0c3945f7ee0e8776
juo <juo6442@gmail.com>
2021-10-18 23:26:19 +0900

    Implement member server: delete

--- solo-play/juo/homework_10/server_member/server.js
+++ solo-play/juo/homework_10/server_member/server.js
@@ -18,7 +18,7 @@ class MemberServer {
             .post(require("./router/member_new.js")(this.store))
             .get(require("./router/member_search.js")(this.store))
             .put(require("./router/member_modify.js")(this.store))
-//            .delete(require("./router/member_delete.js")(this.store));
+            .delete(require("./router/member_delete.js")(this.store));
     }
 
     start() {
