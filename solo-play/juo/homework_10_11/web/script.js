05dc0a1d758fb474fef9e7d317f39766e49b409a
juo <juo6442@gmail.com>
2021-10-23 20:45:22 +0900

    Separate web element script

--- solo-play/juo/homework_10_11/web/script.js
+++ solo-play/juo/homework_10_11/web/script.js
@@ -1,32 +1,5 @@
 const ws = new WebSocket("ws://localhost:12345/member");
-
 const client = new MemberClient("http://localhost:12344/member");
-
-class MemberFormInput {
-    constructor(idElement, passwordElement, nicknameElement) {
-        this.idElement = idElement;
-        this.passwordElement = passwordElement;
-        this.nicknameElement = nicknameElement;
-    }
-
-    get() {
-        return {
-            id: this.idElement.value,
-            password: this.passwordElement.value,
-            nickname: this.nicknameElement.value,
-        }
-    }
-
-    set(id, password, nickname) {
-        this.idElement.value = id;
-        this.passwordElement.value = password;
-        this.nicknameElement.value = nickname;
-    }
-
-    empty() {
-        this.set("", "", "");
-    }
-}
 
 function createMember(e) {
     e.preventDefault();
@@ -165,15 +138,3 @@ function registerEventListeners() {
 }
 
 registerEventListeners();
-
-const joinInput = new MemberFormInput(
-    document.getElementById("join_id"),
-    document.getElementById("join_password"),
-    document.getElementById("join_nickname"),
-);
-
-const manageInput = new MemberFormInput(
-    document.getElementById("manage_id"),
-    document.getElementById("manage_password"),
-    document.getElementById("manage_nickname"),
-);
